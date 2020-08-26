139aeda3c560bb707ad7b67e976343461e6946a7
WESLEY CARLOS DE OLIVEIRA <you@example.comwesley@worc.com.br>
2020-08-25 23:04:29 -0300

    Exercicio 5 - antecessor e sucessor de um numero

---
+++ exe5.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+
+n1 = int(input('Digite um número:'))
+na = n1 - 1
+ns = n1 + 1
+print('O número antecessor de {}, é {}, e o seu sucessor é {}.'.format(n1,na,ns))
+
+# Outra forma de elaborar o código usando apenas uma variavel!!!
+#n1 = int(input('Digite um número:'))
+#print('O número antecessor de {}, é {}, e o seu sucessor é {}.'.format(n1, (n1 - 1),(n1 + 2)))
