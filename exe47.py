17852db2f3c826e560f67d54b9ab4c7c14ff123d
WESLEY CARLOS DE OLIVEIRA <you@example.comwesley@worc.com.br>
2020-08-26 00:18:32 -0300

    Exercicio 47 - todos numeros pares

---
+++ exe47.py
@@ -0,0 +1,8 @@
+#coding: utf-8
+
+#Crie um programa que mostre na tela todos os números pares que estão no intervalo entre 1 e 50.
+print()
+print('Os números pares entre 1 e 50 são: ')
+for c in range(1, 50):
+   if c % 2 == 0:
+       print(c)
