2777065fa061734b19cdc2633da47c2e5765323f
WESLEY CARLOS DE OLIVEIRA <you@example.comwesley@worc.com.br>
2020-08-25 22:59:03 -0300

    Exercicio 3 - soma dos numeros

---
+++ exe3.py
@@ -0,0 +1,6 @@
+# coding: utf-8
+
+v_1 = int(input('Digite um valor '))
+v_2 = int(input('Digite outro valor '))
+result = (v_1) + (v_2)
+print('A soma ente {} e {} é igual a {}!'.format(v_1, v_2, result))
